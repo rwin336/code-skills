4d0de11bf195aab941fd1ce9e3b249a40c38560e
rwin <rwin336@gmail.com>
2022-04-17 08:16:40 -0400

    Adding C# 10 and .Net 6 Chapter 02 Arguments code

---
+++ book-code/cs10dotnet6/chapter02/Arguments/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+ 
+
+WriteLine("");
+WriteLine("Book:    \tC# 10 and .Net6");
+WriteLine("Author:  \tMark J. Price");
+WriteLine("Chapter: \t02");
+WriteLine("Arguments");
+WriteLine("");
+
+WriteLine($"There are {args.Length} arguments");
+foreach(string arg in args)
+{
+    WriteLine(arg);
+}
+
+WriteLine();
+
+if(args.Length < 3) 
+{
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return;
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0],
+    ignoreCase: true
+);
+
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1],
+    ignoreCase: true
+);
+
+try
+{
+    // not supported on Linux
+    CursorSize = int.Parse(args[2]);
+}
+catch(PlatformNotSupportedException)
+{
+    WriteLine("The current Platform does not support changing the size of the cursor");
+}
+
