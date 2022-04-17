f98a7bcfdd59d52695ea2ae238f40209ceb0ec3a
rwin336 <rwin336@gmail.com>
2022-04-17 11:51:24 -0400

    Adding C# 10 and .Net 6 Chapter 03 Boolean Ops

---
+++ book-code/cs10dotnet6/chapter03/BooleanOperators/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+ 
+
+WriteLine("");
+WriteLine("Book:    \tC# 10 and .Net6");
+WriteLine("Author:  \tMark J. Price");
+WriteLine("Chapter: \t03");
+WriteLine("Boolean Operators");
+WriteLine("");
