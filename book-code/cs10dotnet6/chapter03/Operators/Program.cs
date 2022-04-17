3ea6d5b5ad3c366d6e3fa881a8f02f413266838d
rwin336 <rwin336@gmail.com>
2022-04-17 11:24:25 -0400

    Unary operators

--- book-code/cs10dotnet6/chapter03/Operators/Program.cs
+++ book-code/cs10dotnet6/chapter03/Operators/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("");
+WriteLine("Book:    \tC# 10 and .Net6");
+WriteLine("Author:  \tMark J. Price");
+WriteLine("Chapter: \t03");
+WriteLine("Operators");
+WriteLine("");
+
+int a = 3;
+int b = a++;
+
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c;
+
+WriteLine($"c is {c}, d is {d}");
+
+
+
+
+WriteLine();
