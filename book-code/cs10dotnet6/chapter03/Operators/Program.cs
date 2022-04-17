a280f627dac6105ce5d460bd90a6f0795f90db71
Richard Winters <rwin336@gmail.com>
2022-04-17 11:36:16 -0400

    Adding C# 10 and .Net 6 Chapter 03 Operators (#1)
    * Adding C# 10 and .Net 6 Chapter 03 Operators
    
    * Unary operators
    
    * Operators: binary arithmetic

---
+++ book-code/cs10dotnet6/chapter03/Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+WriteLine("");
+WriteLine("Book:    \tC# 10 and .Net6");
+WriteLine("Author:  \tMark J. Price");
+WriteLine("Chapter: \t03");
+WriteLine("Operators");
+WriteLine("");
+
+int a = 3;
+int b = a++;
+
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c;
+
+WriteLine($"c is {c}, d is {d}");
+
+int e = 11;
+int f = 3;
+
+WriteLine();
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+WriteLine();
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
+
+WriteLine();
