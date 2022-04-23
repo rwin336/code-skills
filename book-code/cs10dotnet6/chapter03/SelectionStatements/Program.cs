b04fe9213f713676cb2d6642dd38dd69003f1bb6
rwin336 <rwin336@gmail.com>
2022-04-23 09:21:46 -0400

    If statement is example

--- book-code/cs10dotnet6/chapter03/SelectionStatements/Program.cs
+++ book-code/cs10dotnet6/chapter03/SelectionStatements/Program.cs
@@ -12,6 +12,17 @@ else
     WriteLine("Your password is strong(kind-of).");
 }
 
+object o = 3;
+int j = 4;
+
+if(o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("o is not an int so it cannot multiple!");
+}
 
 
 
