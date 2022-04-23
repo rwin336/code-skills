f75e40fd24ab4636e04768dd0b580826a6e1d1f0
rwin336 <rwin336@gmail.com>
2022-04-23 17:31:36 -0400

    Switch statement goto and label

--- book-code/cs10dotnet6/chapter03/SelectionStatements/Program.cs
+++ book-code/cs10dotnet6/chapter03/SelectionStatements/Program.cs
@@ -25,4 +25,30 @@ else
 }
 
 
+int number = (new Random()).Next(1,7);
+WriteLine($"My random number is {number}");
+
+switch(number)
+{
+
+    case 1:
+        WriteLine("One");
+        break;
+    case 2:
+        WriteLine("Two");
+        goto case 1;
+    case 3:
+    case 4:
+        WriteLine("Three or four");
+        break;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+}
+
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
 
