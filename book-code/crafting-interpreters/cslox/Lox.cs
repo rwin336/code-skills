78d5874f6e3dac6a1582d0d41573f4b0ab4d93a3
rwin336 <rwin336@gmail.com>
2022-04-24 15:18:15 -0400

    C# implementation of lox interpreter

---
+++ book-code/crafting-interpreters/cslox/Lox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using static System.Console;
+
+
+namespace com.craftinginterpreters.lox
+{
+
+
+
+    public class Lox 
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 1) 
+            {
+                WriteLine("Usage: cslox [script]");
+                System.Environment.Exit(64);
+            } 
+            else if(args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+
+        }
+
+
+        private static void RunFile(string path)
+        {
+
+        }
+
+        private static void RunPrompt() 
+        {
+
+        }
+    }
+
+
+
+
+}
