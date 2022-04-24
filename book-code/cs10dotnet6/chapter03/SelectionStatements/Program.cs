2e495089cdb3289500d35656d156c63db3108d67
rwin336 <rwin336@gmail.com>
2022-04-24 06:44:49 -0400

    Covering switch statements

--- book-code/cs10dotnet6/chapter03/SelectionStatements/Program.cs
+++ book-code/cs10dotnet6/chapter03/SelectionStatements/Program.cs
@@ -52,3 +52,73 @@ WriteLine("After end of switch");
 A_label:
 WriteLine($"After A_label");
 
+
+string path = "/home/rwin/ws-code-skils/code-skills/book-code/cs10dotnet6/chapter03";
+
+Write("Press R for read-only or W for writeable: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+
+Stream? s;
+
+if(key.Key == ConsoleKey.R) 
+{
+    s = File.Open(
+        Path.Combine(path, "file.txt"),
+        FileMode.OpenOrCreate,
+        FileAccess.Read
+    );
+}
+else
+{
+    s = File.Open(
+        Path.Combine(path, "file.txt"),
+        FileMode.OpenOrCreate,
+        FileAccess.Write
+    );
+}
+
+string message;
+
+switch(s)
+{
+    case FileStream writeableFile when s.CanWrite:
+        message = "The stream is a file that I can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is a read-only file.";
+        break;
+    case MemoryStream ms:
+        message = "The stream is a memory address.";
+        break;
+    default:
+        message = "The stream is some other type.";
+        break;
+    case null:
+        message = "The stream is null.";
+        break;
+}
+
+WriteLine($"Message: {message}");
+
+double a = 2.0;
+double b = 3.0;
+decimal ad = Convert.ToDecimal(a);
+decimal ab = Convert.ToDecimal(b);
+
+message = s switch 
+{
+    FileStream writeableFile when s.CanWrite
+        => "The stream is a file that I can write to.",
+    FileStream readOnlyFile
+        => "The stream is a read-only file.",
+    MemoryStream ms
+        => "The stream is a memory address.",
+    null
+        => "The stream is null.",
+    _
+        => "The stream is some other type."
+};
+
+
+WriteLine($"Message2: {message}");
