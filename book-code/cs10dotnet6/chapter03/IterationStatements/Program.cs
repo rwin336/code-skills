105a9aa7ed3b1d1db5faddc13395a6624923eeff
rwin336 <rwin336@gmail.com>
2022-04-27 05:31:25 -0400

    Adding C# 10 and .Net 6 Chapter 03 Iteration Statements

---
+++ book-code/cs10dotnet6/chapter03/IterationStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+int x = 0;
+
+while(x < 10)
+{   
+    WriteLine(x);
+    x++;
+}
+
+string? password;
+int i = 0;
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+    i++;
+}
+while(password != "Pa$$w0rd" && i < 10);
+
+if(i < 10) 
+{
+    WriteLine("Correct");
+}
+else
+{
+    WriteLine("Too many password attempts");
+}
+
+
