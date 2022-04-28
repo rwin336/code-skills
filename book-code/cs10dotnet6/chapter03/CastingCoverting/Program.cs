1c7540b6bb10dc2c07667395e516eac0549e5642
rwin336 <rwin336@gmail.com>
2022-04-28 05:20:58 -0400

    Casting numbers implicitly and explicity

--- book-code/cs10dotnet6/chapter03/CastingCoverting/Program.cs
+++ book-code/cs10dotnet6/chapter03/CastingCoverting/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+
+WriteLine("Chapter 03 - Casting Converting");
+
+int a = 10;
+double b = a;
+WriteLine($"b = {b}");
+
+double c = 9.8;
+//int d = c;     // gives compiler error
+int d = (int)c;
+WriteLine($"d = {d}");
+
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}" );
+
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
