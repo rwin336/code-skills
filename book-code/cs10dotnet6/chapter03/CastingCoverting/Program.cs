8244f1d8a14842e4e9bdebabbc185926d0777e0c
rwin336 <rwin336@gmail.com>
2022-04-28 05:58:54 -0400

    String conversion

--- book-code/cs10dotnet6/chapter03/CastingCoverting/Program.cs
+++ book-code/cs10dotnet6/chapter03/CastingCoverting/Program.cs
@@ -48,3 +48,57 @@ foreach(double  n in doubles)
 }
 
 WriteLine();
+
+int number = 12;
+WriteLine(number.ToString());
+
+bool boolean = true;
+WriteLine(boolean.ToString());
+
+DateTime now = DateTime.Now;
+WriteLine(now.ToString());
+
+Object me = new();
+WriteLine(me.ToString());
+
+WriteLine();
+
+// allocate array of 128 bytes
+byte[] binaryObject = new byte[128];
+
+// populate array with random bytes
+(new Random()).NextBytes(binaryObject);
+
+WriteLine("Binary object as bytes:");
+for(int index = 0; index < binaryObject.Length; index++)
+{
+    Write($"{binaryObject[index]:X}");
+}
+WriteLine();
+WriteLine();
+
+// convert to Base64 string and output as text
+string encoded = ToBase64String(binaryObject);
+WriteLine($"Binary Object as Base64:\n{encoded}");
+WriteLine();
+
+int age = int.Parse("27");
+DateTime birthday = DateTime.Parse("4 July 1980");
+
+WriteLine($"I was born {age} years ago");
+WriteLine($"My birthday is {birthday}");
+WriteLine($"My birthday is {birthday:D}");
+
+WriteLine();
+
+Write("How many eggs are there? ");
+string? input = ReadLine();
+
+if(int.TryParse(input, out int count))
+{
+    WriteLine($"There are {count} eggs.");
+}
+else 
+{
+    WriteLine($"I could not parse the input.");
+}
