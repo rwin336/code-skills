47e0b6dffd141420405cbdea5d910839aecc4b3b
Richard Winters <rwin336@gmail.com>
2022-04-30 19:08:53 -0400

    Adding basic unit tests for cslox (#8)

--- book-code/crafting-interpreters/cslox/CsLoxTests/UnitTest1.cs
+++ book-code/crafting-interpreters/cslox/CsLoxTests/UnitTest1.cs
@@ -1,12 +1,103 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.craftinginterpreters.lox;
 
-namespace CsLoxTests;
-
-[TestClass]
-public class UnitTest1
+namespace com.craftinginterpreters.lox.test
 {
-    [TestMethod]
-    public void TestMethod1()
+
+    [TestClass]
+    public class UnitTest1
     {
+
+        [TestMethod]
+        public void TestParens()
+        {
+            Assert.IsFalse(Lox.scan("() (())"));
+        }
+
+        [TestMethod]
+        public void TestBraces()
+        {
+            Assert.IsFalse(Lox.scan("{} {{}}"));
+        }
+
+        [TestMethod]
+        public void TestCommas()
+        {
+            Assert.IsFalse(Lox.scan(",,,,,"));
+        }
+
+        [TestMethod]
+        public void TestDots()
+        {
+            Assert.IsFalse(Lox.scan(".,.,...."));
+        }
+
+        [TestMethod]
+        public void TestMinusSign()
+        {
+            Assert.IsFalse(Lox.scan("-----"));
+        }
+
+        [TestMethod]
+        public void TestPlusSign()
+        {
+            Assert.IsFalse(Lox.scan("+++++"));
+        }
+
+        [TestMethod]
+        public void TestSemiColon()
+        {
+            Assert.IsFalse(Lox.scan(";;;;;"));
+        }
+
+        [TestMethod]
+        public void TestStar()
+        {
+            Assert.IsFalse(Lox.scan("*****"));
+        }
+
+
+        [TestMethod]
+        public void TestNumberAssignmentStatement()
+        {
+            Assert.IsFalse(Lox.scan("var = 1234.56"));
+        }
+
+        [TestMethod]
+        public void TestStringAssignmentStatement()
+        {
+            Assert.IsFalse(Lox.scan("var = \"Test string\""));
+        }
+
+        [TestMethod]
+        public void TestQuotedString()
+        {
+            Assert.IsFalse(Lox.scan("\"This string is quoted properly\""));
+        }
+
+        [TestMethod]
+        public void TestUnterminatedString()
+        {
+            Assert.IsTrue(Lox.scan("\"This string is not quoted properly"));
+        }
+
+        [TestMethod]
+        public void TestUnterminatedStringAssignmentStatement()
+        {
+            Assert.IsTrue(Lox.scan("var = \"Test string"));
+        }
+
+        [TestMethod]
+        public void TestCharMix()
+        {
+            Assert.IsFalse(Lox.scan("(*=-+{!=}==.)"));
+        }
+
+        [TestMethod]
+        public void TestCharMixQuoted()
+        {
+            Assert.IsFalse(Lox.scan("\"(*=-+{!=}==.)\""));
+        }
+
     }
 }
