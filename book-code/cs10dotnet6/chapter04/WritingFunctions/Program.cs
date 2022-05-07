2fb92a172e750253fe05b306b5fea57120dfbcb9
rwin336 <rwin336@gmail.com>
2022-05-07 11:26:02 -0400

    Adding CalculatingTax

--- book-code/cs10dotnet6/chapter04/WritingFunctions/Program.cs
+++ book-code/cs10dotnet6/chapter04/WritingFunctions/Program.cs
@@ -10,13 +10,55 @@ namespace com.rwin.codeskills.cs10dotnet6.chap4.writingfunctions
         public static void Main(string[] args)
         {
 
-            TimesTable(number: 6);
-        
+            //TimesTable(number: 6);
+            decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+            WriteLine($"You must pay {taxToPay} in tax.");
 
 
         }
 
 
+        static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+        {
+            decimal rate = 0.0M;
+
+            switch(twoLetterRegionCode)
+            {
+                case "CH":  //SwitzerLand
+                    rate = 0.08M;
+                    break;
+                case "DK":  // Denmark
+                case "NO":  // Norway
+                    rate = 0.25M;
+                    break;
+                case "GB":  // United Kingdom
+                case "FR":  // France
+                    rate = 0.2M;
+                    break;
+                case "HU":  // Hungary
+                    rate = 0.27M;
+                    break;
+                case "OR":  // Oregon
+                case "AK":  // Alaska
+                case "MT":  // Montana
+                    rate = 0.0M;
+                    break;
+                case "ND":  // North Dakota
+                case "WI":  // Wisconsin
+                case "ME":  // Maine
+                case "VA":  // Virgina
+                    rate = 0.05M;
+                    break;
+                case "CA":  // California
+                    rate = 0.0825M;
+                    break;
+                default:    // Most US States
+                    rate = 0.06M;
+                    break;
+            }
+
+            return amount * rate ;
+        }
 
         public static void TimesTable(byte number)
         {
