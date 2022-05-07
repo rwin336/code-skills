211701250b81015fba8d1bc11126afe51e4eabe0
rwin336 <rwin336@gmail.com>
2022-05-07 12:18:56 -0400

    Adding Factorial

--- book-code/cs10dotnet6/chapter04/WritingFunctions/Program.cs
+++ book-code/cs10dotnet6/chapter04/WritingFunctions/Program.cs
@@ -15,9 +15,46 @@ namespace com.rwin.codeskills.cs10dotnet6.chap4.writingfunctions
             //decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
             //WriteLine($"You must pay {taxToPay} in tax.");
 
-            RunCardinalToOrdianl();
+            //RunCardinalToOrdianl();
+
+            RunFactorial();
 
         }
+
+        static void RunFactorial()
+        {
+            for(int i = 1; i < 15; i++)
+            {
+                try {
+                    WriteLine($"{i}! = {Factorial(i):N0}");
+                }
+                catch(System.OverflowException)
+                {
+                    WriteLine($"{i}! is too big for a 32-bit integer");
+                }
+            }
+        }
+
+        static int Factorial(int number)
+        {
+            if(number < 1)
+            {
+                return 0;
+            }
+            else if(number == 1)
+            {
+                return 1;
+            }
+            else 
+            {
+                checked 
+                {
+                    return number * Factorial(number - 1);
+                }
+            }
+        }
+
+
 
         static void RunCardinalToOrdianl()
         {
