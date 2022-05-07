559ef423e85487ac673a16bcbaaf4a768fd2286f
Richard Winters <rwin336@gmail.com>
2022-05-07 17:47:56 -0400

    Adding C# 10 and .Net 6 Chapter 04 Debugging (#16)
    * Adding C# 10 and .Net 6 Chapter 04 Debugging
    
    * Implement debugging

---
+++ book-code/cs10dotnet6/chapter04/Debugging/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using static System.Console;
+
+namespace com.rwin.codeskills.cs10dotnet6.chap4.debugging
+{
+    public class Debugging
+    {
+        static void Main(string[] args)
+        {
+            double a = 4.5;
+            double b = 2.5;
+            double answer = Add(a, b);
+
+            WriteLine("Press ENTER to end the app");
+            ReadLine();
+        }
+
+
+        static double Add(double a, double b)
+        {
+            return a * b;  // deliberate bug
+        }
+
+    }
+}
