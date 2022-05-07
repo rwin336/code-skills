1b85a0b4a9bf6be8a46ff876022b0c25fb36dc92
rwin336 <rwin336@gmail.com>
2022-05-07 11:11:58 -0400

    Adding Times table

--- book-code/cs10dotnet6/chapter04/WritingFunctions/Program.cs
+++ book-code/cs10dotnet6/chapter04/WritingFunctions/Program.cs
@@ -10,12 +10,24 @@ namespace com.rwin.codeskills.cs10dotnet6.chap4.writingfunctions
         public static void Main(string[] args)
         {
 
-            WriteLine("Hello from Writing Functions");
+            TimesTable(number: 6);
         
 
 
         }
 
+
+
+        public static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table");
+
+            for(int row = 1; row <= 12; row++)
+            {
+                WriteLine($"{row} x {number} = {row * number}");
+            }
+            WriteLine("");
+        }
 
     }
 
