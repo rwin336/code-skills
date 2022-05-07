f24f38736f99edcc8ecef3ec1a68fea37d73a4a9
rwin336 <rwin336@gmail.com>
2022-05-07 17:47:05 -0400

    Implement debugging

--- book-code/cs10dotnet6/chapter04/Debugging/Program.cs
+++ book-code/cs10dotnet6/chapter04/Debugging/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using static System.Console;
+
+namespace com.rwin.codeskills.cs10dotnet6.chap4.debugging
+{
+    public class Debugging
+    {
+        static void Main(string[] args)
+        {
+            double a = 4.5;
+            double b = 2.5;
+            double answer = Add(a, b);
+
+            WriteLine("Press ENTER to end the app");
+            ReadLine();
+        }
+
+
+        static double Add(double a, double b)
+        {
+            return a * b;  // deliberate bug
+        }
+
+    }
+}
