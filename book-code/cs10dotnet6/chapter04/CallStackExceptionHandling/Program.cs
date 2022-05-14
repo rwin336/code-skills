da4521d49c5f91d60801b3b8097c300ef6f5a9e9
rwin336 <rwin336@gmail.com>
2022-05-14 05:13:53 -0400

    Adding C# 10 and .Net 6 Chapter 04 call-stack

---
+++ book-code/cs10dotnet6/chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Packet;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+static void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+static void Beta()
+{
+    WriteLine("In Beta");
+    Calculator.Gamma();
+}
