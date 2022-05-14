b7858df18072587dd3207bb0f0f079f139c5f958
Richard Winters <rwin336@gmail.com>
2022-05-14 05:40:24 -0400

    Adding C# 10 and .Net 6 Chapter 04 call-stack (#19)
    * Adding C# 10 and .Net 6 Chapter 04 call-stack
    
    * Catching exceptions examples

---
+++ book-code/cs10dotnet6/chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Packet;
+using Microsoft.Extensions.Logging;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+static void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+static void Beta()
+{
+    WriteLine("In Beta");
+
+    try 
+    {
+        Calculator.Gamma();
+    }
+    catch(IOException ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        throw ex;
+
+        //LogException(ex);
+        //
+        //throw ex;
+        //
+        //throw;
+        //
+        //throw new InvalidOperationException(
+        //    message: "Calculation had invalid values.  See inner exception for why.",
+        //    innerException: ex
+        //);
+    }
+}
---
+++ book-code/cs10dotnet6/chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace Packet;
+public class Calculator
+{
+    public static void Gamma()
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    public static void Delta()
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
