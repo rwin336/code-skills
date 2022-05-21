25bcb97accb07c3b48c5814adab5fe09d9f7d32a
rwin336 <rwin336@gmail.com>
2022-05-21 09:54:26 -0400

    Setting and outputting field values

--- book-code/cs10dotnet6/chapter05/PacktLibrary/Person.cs
+++ book-code/cs10dotnet6/chapter05/PacktLibrary/Person.cs
@@ -3,9 +3,11 @@ using static System.Console;
 
 namespace Packt.Shared;
 
-public class Person
+public class Person : object
 {
-
+    // fields
+    public string Name;
+    public DateTime DateOfBirth;
 
 
 }
--- book-code/cs10dotnet6/chapter05/PeopleApp/Program.cs
+++ book-code/cs10dotnet6/chapter05/PeopleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+// var bob = new Person();  // C# 1.0 or later
+Person bob = new();         // C# 9.0 or later
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+WriteLine(bob.ToString());
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}", 
+    arg0: bob.Name, 
+    arg1: bob.DateOfBirth);
+
+Person alice = new() 
+{
+    Name = "Alice Jones",
+    DateOfBirth = new(1998, 3, 7)
+};
+
+WriteLine(format: "{0} was born on {1:dd MMM yy}",
+    arg0: alice.Name,
+    arg1: alice.DateOfBirth);
