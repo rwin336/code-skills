18c2cfe6c92e49fcabc14bb937f63814b05f44a2
rwin336 <rwin336@gmail.com>
2022-05-22 18:42:03 -0400

    Making a field static

--- book-code/cs10dotnet6/chapter05/PeopleApp/Program.cs
+++ book-code/cs10dotnet6/chapter05/PeopleApp/Program.cs
@@ -42,3 +42,27 @@ Person alice = new()
 WriteLine(format: "{0} was born on {1:dd MMM yy}",
     arg0: alice.Name,
     arg1: alice.DateOfBirth);
+WriteLine();
+
+BankAccount.InterestRate = 0.012M;  // store a shared value
+
+BankAccount jonesAccount = new();  // C# 9.0 or later
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+
+WriteLine(
+    format: "{0} earned {1:C} interest.",
+    arg0: jonesAccount.AccountName,
+    arg1: jonesAccount.Balance * BankAccount.InterestRate
+);
+
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+
+WriteLine(
+    format: "{0} earned {1:C} interest.",
+    arg0: gerrierAccount.AccountName,
+    arg1: gerrierAccount.Balance * BankAccount.InterestRate
+);
+
