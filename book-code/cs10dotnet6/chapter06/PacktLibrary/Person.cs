e13aeacb77b33ae3f7f3e7750358d57d32b4672a
rwin336 <rwin336@gmail.com>
2022-06-06 06:19:13 -0400

    Adding C# 10 and .Net 6 Chapter 06 Classlib

---
+++ book-code/cs10dotnet6/chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace Packt.Shared; 
+
+
+public class Person : object
+{
+
+    public string? Name;        // ? allows null
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();   // C# 9 or later    
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+}
