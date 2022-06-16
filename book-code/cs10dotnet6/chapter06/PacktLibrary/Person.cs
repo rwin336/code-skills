665845e9d8d55eafad840c8c0285c26e2e5ce9ae
rwin336 <rwin336@gmail.com>
2022-06-16 04:47:14 -0400

    Implementing functionality using methods

--- book-code/cs10dotnet6/chapter06/PacktLibrary/Person.cs
+++ book-code/cs10dotnet6/chapter06/PacktLibrary/Person.cs
@@ -10,6 +10,25 @@ public class Person : object
     public DateTime DateOfBirth;
     public List<Person> Children = new();   // C# 9 or later    
 
+    // static method to "multiply"
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+    }
+
+    // instance method to "multiply"
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
     // methods
     public void WriteToConsole()
     {
--- book-code/cs10dotnet6/chapter06/PeopleApp/Program.cs
+++ book-code/cs10dotnet6/chapter06/PeopleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+using static System.Console;
+
+Person harry = new() { Name = "Harry"};
+Person mary = new() { Name = "Mary"};
+Person jill = new() { Name = "Jill"};
+
+// call instance method
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+// call static method
+Person baby2 = Person.Procreate(harry, jill);
+
+WriteLine($"{harry.Name} has {harry.Children.Count} children");
+WriteLine($"{mary.Name} has {mary.Children.Count} children");
+WriteLine($"{jill.Name} has {jill.Children.Count}");
+
+WriteLine(
+    format: "{0}'s first child is named \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children[0].Name
+);
