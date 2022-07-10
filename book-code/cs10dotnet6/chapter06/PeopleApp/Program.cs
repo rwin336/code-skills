7bfe990f398f08793ac1c78aa0872f1ac22cabc9
rwin336 <rwin336@gmail.com>
2022-07-10 07:06:03 -0400

    Defining struct types

--- book-code/cs10dotnet6/chapter06/PeopleApp/Program.cs
+++ book-code/cs10dotnet6/chapter06/PeopleApp/Program.cs
@@ -126,3 +126,13 @@ foreach(Person p in people)
 {
     WriteLine($"    {p.Name}");
 }
+
+
+DisplacementVector dv1 = new(3, 5);
+DisplacementVector dv2 = new(-2, 7);
+DisplacementVector dv3 = dv1 + dv2;
+
+WriteLine();
+WriteLine("Displacement Vector Addition");
+WriteLine($"({dv1.X}, {dv1.Y}) + ({dv2.X}, {dv2.Y}) = ({dv3.X}, {dv3.Y})");
+
