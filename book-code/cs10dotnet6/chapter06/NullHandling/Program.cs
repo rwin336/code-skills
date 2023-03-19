32c0682cfa5c72bdd4123df946dcbd7eda6c83a1
rwin336 <rwin336@gmail.com>
2023-03-19 07:32:21 -0400

    Adding NullHandling project

---
+++ book-code/cs10dotnet6/chapter06/NullHandling/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+int thisCannotBeNull = 4;
+//thisCannotBeNull = null;
+
+int? thisCouldBeNull = null;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+thisCouldBeNull = 7;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
