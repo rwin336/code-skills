9ddd88915f6d20722ff4dc9c189bb944543ae36d
rwin336 <rwin336@gmail.com>
2023-03-19 08:05:58 -0400

    Adding NullHandling project/code

--- book-code/cs10dotnet6/chapter06/NullHandling/Program.cs
+++ book-code/cs10dotnet6/chapter06/NullHandling/Program.cs
@@ -11,3 +11,21 @@ thisCouldBeNull = 7;
 WriteLine(thisCouldBeNull);
 WriteLine(thisCouldBeNull.GetValueOrDefault());
 
+
+Address address = new Address();
+address.Building = null;
+address.Street = null;
+address.City = "London";
+address.Region = null;
+
+class Address
+{
+  public string? Building;
+  public string Street = string.Empty;
+  public string City = string.Empty;
+  public string Region = string.Empty;
+}
+
+
+
+
